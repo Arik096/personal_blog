8da39cd274995dd597c9901d873ca659b689d329
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-20 20:44:31 +0600

    Create thesis.php

---
+++ thesis.php
@@ -0,0 +1,23 @@
+<?php include 'lib/bootstrap.php'; ?>
+<?php include 'lib/header.php'; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Arik - Thesis</title>
+  </head>
+  <body>
+
+    <div class="jumbotron">
+  <h1 class="display-4">ArNet</h1>
+  <p class="lead">A Deep Machine Learning Architecture for Semantic Image Segmentation.</p>
+  <p class="lead">
+    <hr class="my-5">
+    <a class="btn btn-primary btn-lg"  href="pdf/ArNet.pdf" role="button" onclick="func1()">Journal paper</a>
+    <a class="btn btn-primary btn-lg"  href="pdf/SegNet.pdf" role="button" onclick="func2()">Thesis book</a>
+  </p>
+</div>
+  </body>
+</html>
+
+<?php include 'lib/footer.php'; ?>
