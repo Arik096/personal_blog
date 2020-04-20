f2ba2378fea2af368bbc8ed61a2e4a40b67d0db5
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-20 20:44:13 +0600

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -4,49 +4,51 @@
     <link rel="stylesheet" href=".//css/header.css">
   </head>
   <body>
+    <div class="sticky-top">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
     <img class="img" src=".//img/my_image.jpg" width="70px" height="70px" alt="my_image">
   </a>
-      <a class="navbar-brand" href="#">Arik Md. Isthiaque</a>
+      <a class="navbar-brand" href="admin.php">Arik Md. Isthiaque</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="index.php">Blog<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="alma_mater.php">Alma Mater</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Technical Skills</a>
+            <a class="nav-link" href="tech_skill.php">Technical Skills</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Thesis</a>
+            <a class="nav-link" href="thesis.php">Thesis</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Projects</a>
+            <a class="nav-link" href="project.php">Projects</a>
           </li>
+          <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Achievements
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Awards</a>
+              <a class="dropdown-item"  href="award.php">Awards</a>
               <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Certificates</a>
+              <a class="dropdown-item"  href="certificate.php">Certificates</a>
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Memberships</a>
+            <a class="nav-link" href="membership.php">Memberships</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Extra Curriculum</a>
+            <a class="nav-link" href="extra.php">Extra Curriculum</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">References</a>
+            <a class="nav-link" href="references.php">References</a>
           </li>
         </ul>
         <a class="navbar-brand" href="https://www.facebook.com/arik.isthiaque" target="_blank">
@@ -60,6 +62,6 @@
 </a>
       </div>
     </nav>
-
+</div>
   </body>
 </html>
