03d858d3bb21411f8ae6bcd1bcb40a5ee6ea928a
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-20 15:42:50 +0600

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -1,1 +1,58 @@
-<h1>header</h1
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href=".//css/header.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+    <img class="img" src=".//img/my_image.jpg" width="70px" height="70px" alt="my_image">
+  </a>
+      <a class="navbar-brand" href="#">Arik Md. Isthiaque</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Blog<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Amla Mater</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Technical Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Thesis</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Projects</a>
+          </li>
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Achievements
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Awards</a>
+              <a class="dropdown-item" href="#">Certificates</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Memberships</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Extra Curriculum</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Personal Infromation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">References</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  </body>
+</html>
