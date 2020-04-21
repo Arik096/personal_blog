d1077e22497eba786465ed1d3e863ab8abbcc121
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-21 16:32:37 +0600

    Update project.php

--- project.php
+++ project.php
@@ -1,4 +1,177 @@
 <?php include 'lib/bootstrap.php'; ?>
 <?php include 'lib/header.php'; ?>
-<h1>project</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arik - projects</title>
+  </head>
+  <body>
+
+
+    <div id="accordion">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              BAIUST Hall Management
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            using basic PHP and MySQL database
+            <br>
+            <br>
+            <a class="btn btn-info" href="http://baiusthall.byethost13.com/" target="_blank">Live Demo</a>
+            <a class="btn btn-info" href="https://github.com/Arik096/baiusthall" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Personal Portfolio
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            Using basic PHP and MySQL database
+            <br>
+            <br>
+            <a class="btn btn-info" href="http://baiusthall.byethost13.com/" target="_blank">Live Demo</a>
+            <a class="btn btn-info" href="https://github.com/Arik096/personal_blog" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingThree">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Shop Management System
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+          <div class="card-body">
+            Using Android Studio and SQLite database
+            <br>
+            <br>
+            <a class="btn btn-info" href="https://github.com/Arik096/kungfuPanda" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingFour">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+              Library Management System
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+          <div class="card-body">
+            Using Android Studio and SQLite database
+            <br>
+            <br>
+            <a class="btn btn-info" href="https://github.com/Arik096/BAIUST_LMS" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingFive">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+              Hospital Management System
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+          <div class="card-body">
+            Using Java swing framework and MySQL database
+            <br>
+            <br>
+            <a class="btn btn-info" href="https://github.com/Arik096/Hospital-Management-System" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingSix">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+              City bridge project
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+          <div class="card-body">
+            Using C++ OpenGL library to implement basic modeling transformations
+            <br>
+            <br>
+            <a class="btn btn-info" href="https://github.com/Arik096/Graphics-LAB/tree/master/city_bridge_project" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingSeven">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+              Result Management System
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+          <div class="card-body">
+            Using basic PHP and MySQL database
+            <br>
+            <br>
+            <a class="btn btn-info" href="https://github.com/Arik096/Result-management-system" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingEight">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+              Temperature controlled DC fan project
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordion">
+          <div class="card-body">
+            Using ATmega32 micro-controller
+            <br>
+            <br>
+            <a class="btn btn-info" href="https://github.com/Arik096/MicroController-MicroProcessor" target="_blank">GitHub</a>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+
+
+  </body>
+</html>
 <?php include 'lib/footer.php'; ?>
