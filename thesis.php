26e9dd8e3f63f2ab2525d9e07cfc277e3cae4e83
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-21 15:09:28 +0600

    Update thesis.php

--- thesis.php
+++ thesis.php
@@ -8,13 +8,17 @@
   </head>
   <body>
 
-    <div class="jumbotron">
-  <h1 class="display-4">ArNet</h1>
+    <div class="jumbotron" style="height: 450px;">
+  <h1 class="display-6">ArNet</h1>
   <p class="lead">A Deep Machine Learning Architecture for Semantic Image Segmentation.</p>
   <p class="lead">
     <hr class="my-5">
-    <a class="btn btn-primary btn-lg"  href="pdf/ArNet.pdf" role="button" onclick="func1()">Journal paper</a>
-    <a class="btn btn-primary btn-lg"  href="pdf/SegNet.pdf" role="button" onclick="func2()">Thesis book</a>
+    <a class="btn btn-primary btn-lg"  href="pdf/ArNet.pdf" target="_blank" role="button" >Journal paper</a>
+    <br>
+    <hr class="my-4">
+    <br>
+    <a class="btn btn-primary btn-lg"  href="pdf/SegNet.pdf" target="_blank" role="button" >Thesis book</a>
+
   </p>
 </div>
   </body>
