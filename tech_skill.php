ff9c15ef2b37d5a48e734a1c99aed204769f7769
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-21 16:32:38 +0600

    Update tech_skill.php

--- tech_skill.php
+++ tech_skill.php
@@ -7,7 +7,61 @@
   </head>
   <body>
 
-
+<div class="card-deck" style="height:500px;">
+    <div class="card border-primary pt-2">
+  <div class="card-header">
+    <h2 style="text-align:center">Languages</h2>
+  </div>
+  <div class="card-body">
+    <ul style="font-size:20px">
+      <li>C++</li>
+      <li>C</li>
+      <li>Java</li>
+      <li>Python</li>
+      <li>HTML 5</li>
+      <li>CSS 3</li>
+      <li>PHP 7</li>
+      <li>JavaScript ES6</li>
+      <li>Prolog</li>
+    </ul>
+  </div>
+</div>
+<div class="card border-primary pt-2">
+<div class="card-header">
+<h2 style="text-align:center">IDE & Tools</h2>
+</div>
+<div class="card-body">
+  <ul style="font-size:20px">
+    <li>GitHub</li>
+    <li>Code Blocks</li>
+    <li>MATLAB</li>
+    <li>Android Studio</li>
+    <li>Atom</li>
+    <li>NetBeans</li>
+    <li>Autodesk AutoCAD 2007</li>
+    <li>Sublime Text</li>
+    <li>VMware</li>
+    <li>Arduino</li>
+    <li>Raspberry Pi</li>
+  </ul>
+</div>
+</div>
+<div class="card border-primary pt-2">
+<div class="card-header">
+<h2 style="text-align:center">Database & Tools</h2>
+</div>
+<div class="card-body">
+  <ul style="font-size:20px">
+    <li>Oracle</li>
+    <li>MySQL</li>
+    <li>MATLAB</li>
+    <li>phpMyAdmin</li>
+    <li>XAMPP</li>
+    <li>SQLite</li>
+  </ul>
+</div>
+</div>
+</div>
 
 
 
