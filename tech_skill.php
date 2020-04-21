ad72d79a1eadad5e350e82930b4a2bff3c3267b5
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-21 15:09:26 +0600

    Update tech_skill.php

--- tech_skill.php
+++ tech_skill.php
@@ -1,4 +1,16 @@
 <?php include 'lib/bootstrap.php'; ?>
 <?php include 'lib/header.php'; ?>
-<h1>tech_skill</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arik - Tech Skill</title>
+  </head>
+  <body>
+
+
+
+
+
+  </body>
+</html>
 <?php include 'lib/footer.php'; ?>
