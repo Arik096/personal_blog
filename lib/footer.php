61db9ed873988566ff4afb989c942b829245fe9b
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-21 15:09:24 +0600

    Update footer.php

--- lib/footer.php
+++ lib/footer.php
@@ -1,29 +1,40 @@
-<div class="fixed-bottom">
-<div class="bg-dark mt-5 footer">
-  <hr class="bg-primary">
-  <h4 class="text-white text-center mt-2">Developed By</h4>
-  <hr class="bg-primary">
-        <div class="row">
-    <div class="col-sm-6">
-  <div class="text-center">
-    <h4 class="text-white text-center mt-2">Arik Md. Isthiaque</h4>
-    <a class="navbar-brand" href="https://www.facebook.com/arik.isthiaque" target="_blank">
-  <img class="img" src="img/fb.png" width="30px" height="30px" alt="fb_image">
-</a>
-<a class="navbar-brand" href="https://www.linkedin.com/in/arik1155001/" target="_blank">
-<img class="img" src="img/linkedin.png" width="35px" height="35px" alt="linkedin_image">
-</a>
-<a class="navbar-brand" href="https://github.com/Arik096" target="_blank">
-<img class="img" src="img/github.jpg" width="30px" height="30px" alt="github_image">
-</a>
-  </div>
-  </div>
-  <div class="col-sm-6">
-<div class="text-center">
-  <p class="text-white"><span class="footer-name" >Gmail: <a href="mailto:arikbncc@gmail.com.com?Subject=Hello" target="_top">arikbncc@gmail.com</a></span><br></p>
-  <p class="text-white"><span class="footer-name" >Phone: +8801521493029</span><br></p>
-</div>
-</div>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+
+    <div class="fixed-bottom">
+    <div class="bg-dark mt-5 footer">
+      <hr class="bg-primary">
+      <h4 class="text-white text-center mt-2">Developed By</h4>
+      <hr class="bg-primary">
+            <div class="row">
+        <div class="col-sm-6">
+      <div class="text-center">
+        <h4 class="text-white text-center mt-2">Arik Md. Isthiaque</h4>
+        <a class="navbar-brand" href="https://www.facebook.com/arik.isthiaque" target="_blank">
+      <img class="img" src="img/fb.png" width="30px" height="30px" alt="fb_image">
+    </a>
+    <a class="navbar-brand" href="https://www.linkedin.com/in/arik1155001/" target="_blank">
+    <img class="img" src="img/linkedin.png" width="35px" height="35px" alt="linkedin_image">
+    </a>
+    <a class="navbar-brand" href="https://github.com/Arik096" target="_blank">
+    <img class="img" src="img/github.jpg" width="30px" height="30px" alt="github_image">
+    </a>
+      </div>
+      </div>
+      <div class="col-sm-6">
+    <div class="text-center">
+      <p class="text-white"><span class="footer-name" >Gmail: <a href="mailto:arikbncc@gmail.com.com?Subject=Hello" target="_top">arikbncc@gmail.com</a></span><br></p>
+      <p class="text-white"><span class="footer-name" >Phone: +8801521493029</span><br></p>
     </div>
-</div>
-</div>
+    </div>
+        </div>
+    </div>
+    </div>
+
+
+
+  </body>
+</html>
