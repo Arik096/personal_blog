da89ab5ddf67f7c012a22a65a82cbda38d1dd076
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-21 15:09:16 +0600

    Update alma_mater.php

--- alma_mater.php
+++ alma_mater.php
@@ -1,4 +1,41 @@
 <?php include 'lib/bootstrap.php'; ?>
 <?php include 'lib/header.php'; ?>
-<h1>Alma mater</h1>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arik - Alma Mater</title>
+  </head>
+  <body>
+
+    <div class="card-deck" style="height:600px;">
+      <div class="card border-primary" >
+      <img class="card-img-top" src="img/baiust.gif"  width="100px" height="350px" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">B.Sc. in Computer Science and Engineering (March, 2016 - April, 2020 expected)</h5>
+        <p class="card-text">Bangladesh Army International University of Science and Technology, Cumilla Cantonment, Bangladesh</p>
+        <a href="https://www.baiust.edu.bd/" target="_blank" class="btn btn-primary">BAIUST Website</a>
+      </div>
+    </div>
+    <div class="card border-primary">
+    <img class="card-img-top" src="img/ndc.jpg" width="100px" height="350px" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Higher Secondary Certificate (2013 - 2015)</h5>
+      <p class="card-text">Notre Dame College, Dhaka, Bangladesh</p>
+      <a href="https://notredamecollege-dhaka.com/" target="_blank" class="btn btn-primary">NDC Website</a>
+    </div>
+  </div>
+  <div class="card border-primary">
+  <img class="card-img-top" src="img/acps.jpg" width="100px" height="350px" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Secondary School Certificate (2011 - 2013)</h5>
+    <p class="card-text">Adamjee Cantonment Public School, Dhaka Cantonment, Bangladesh</p>
+    <a href="https://www.acps.edu.bd/" target="_blank" class="btn btn-primary">ACPS Website</a>
+  </div>
+</div>
+</div>
+
+  </body>
+</html>
+
 <?php include 'lib/footer.php'; ?>
