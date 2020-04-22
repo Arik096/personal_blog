20376144af08e9a2589aaa93d88463fa1306f0cb
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-22 14:58:55 +0600

    Update certificate.php

--- certificate.php
+++ certificate.php
@@ -1,4 +1,142 @@
 <?php include 'lib/bootstrap.php'; ?>
 <?php include 'lib/header.php'; ?>
-<h1>certificate</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arik - Certificates</title>
+  </head>
+  <body>
+
+    <div id="accordion">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              C++ Tutorial Course – SOLOLERAN
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/C++.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              HTML Fundamental Course – SOLOLERAN
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/HTML.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingThree">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              JAVA Tutorial Course – SOLOLERAN
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/JAVA.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingFour">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+              SQL Fundamental Course – SOLOLERAN
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/SQL.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingFive">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+              PHP & MySQL - Certification Course – Udemy
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/PHP and MySQL Udemy.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingSix">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+              Neural Networks (ANN) using Keras and TensorFlow in Python – Udemy
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/ANN Udemy.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingSeven">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+              Machine Learning 101: Introduction to Machine Learning – Udemy
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/ML 100 Udemy.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
 <?php include 'lib/footer.php'; ?>
