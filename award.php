422983188c895ec46514dc914528a6668cb3ad31
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-22 14:58:53 +0600

    Update award.php

--- award.php
+++ award.php
@@ -1,4 +1,207 @@
 <?php include 'lib/bootstrap.php'; ?>
 <?php include 'lib/header.php'; ?>
-<h1>award</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arik - Awards</title>
+  </head>
+  <body>
+
+    <div id="accordion">
+      <div class="card">
+        <div class="card-header" id="headingOne">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Honorable Mention – The 2017 ACM-ICPC Asia Dhaka Regional Contest
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/2017_CertificateContestantPlace_Asia_Dhaka_2017_404779.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Honorable Mention – The 2018 ACM-ICPC Bangladesh IUBAT NCPC Provincial
+Programming Contest
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/2018_CertificateContestantPlace_Bangladesh_IUBAT_NCPC_450690.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingThree">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Honorable Mention – The 2017 ACM-ICPC Bangladesh CUET NCPC Provincial
+Programming Contest
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/2017_CertificateContestantPlace_Bangladesh_CUET_NCPC_355080.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingFour">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+              Galactic Problem Solver – International Space App Challenge (NASA Space App Challenge
+2017, Dhaka)
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/nasa.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingFive">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+              Champion – BCC Day 2016 Intra University Programming Contest, BAIUST
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/bcc 1st.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingSix">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+              IUBAT IUPC 2016
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/iubat.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingSeven">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+              IUT IUPC 2017
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/iut.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="card">
+        <div class="card-header" id="headingEight">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+              BACS-BUBT National Programming Camp 2017
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/npc 17.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="headingNine">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+              Perfect Attendance Certificate 2015 – Notre Dame College
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/perfect attendence.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="card">
+        <div class="card-header" id="headingTen">
+          <h5 class="mb-0">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+              Best Student Performance Award 2013, BSB Foundation
+            </button>
+          </h5>
+        </div>
+
+        <div id="collapseTen" class="collapse" aria-labelledby="headingTen" data-parent="#accordion">
+          <div class="card-body">
+            <div class="embed-responsive embed-responsive-16by9">
+                 <iframe class="embed-responsive-item" src="pdf/cambrian.pdf"></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+
+
+  </body>
+</html>
 <?php include 'lib/footer.php'; ?>
