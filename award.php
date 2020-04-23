6cc8cc750bbdbef91abe64daa52b9dd4454b8926
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-23 14:50:04 +0600

    Update award.php

--- award.php
+++ award.php
@@ -11,7 +11,7 @@
       <div class="card">
         <div class="card-header" id="headingOne">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Honorable Mention – The 2017 ACM-ICPC Asia Dhaka Regional Contest
             </button>
           </h5>
@@ -29,7 +29,7 @@
       <div class="card">
         <div class="card-header" id="headingTwo">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Honorable Mention – The 2018 ACM-ICPC Bangladesh IUBAT NCPC Provincial
 Programming Contest
             </button>
@@ -48,7 +48,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingThree">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Honorable Mention – The 2017 ACM-ICPC Bangladesh CUET NCPC Provincial
 Programming Contest
             </button>
@@ -67,7 +67,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingFour">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Galactic Problem Solver – International Space App Challenge (NASA Space App Challenge
 2017, Dhaka)
             </button>
@@ -87,7 +87,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingFive">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Champion – BCC Day 2016 Intra University Programming Contest, BAIUST
             </button>
           </h5>
@@ -106,7 +106,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingSix">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               IUBAT IUPC 2016
             </button>
           </h5>
@@ -125,7 +125,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingSeven">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               IUT IUPC 2017
             </button>
           </h5>
@@ -144,7 +144,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingEight">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               BACS-BUBT National Programming Camp 2017
             </button>
           </h5>
@@ -162,7 +162,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingNine">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Perfect Attendance Certificate 2015 – Notre Dame College
             </button>
           </h5>
@@ -182,7 +182,7 @@ Programming Contest
       <div class="card">
         <div class="card-header" id="headingTen">
           <h5 class="mb-0">
-            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
               Best Student Performance Award 2013, BSB Foundation
             </button>
           </h5>
