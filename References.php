ee808567833f68e1237c72d22f52809236e05f32
Arik Md. Isthiaque <arikbncc@gmail.com>
2020-04-23 14:50:12 +0600

    Update References.php

--- References.php
+++ References.php
@@ -1,4 +1,69 @@
 <?php include 'lib/bootstrap.php'; ?>
 <?php include 'lib/header.php'; ?>
-<h1>references</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Arik - Alma Mater</title>
+  </head>
+  <body>
+
+<div class="pt-5">
+
+
+    <div class="card-deck" style="height:inherit">
+      <div class="card border-primary px-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
+      <div class="card-body" style="text-align:center;">
+        <h5 class="card-title">Mohammad Asaduzzaman Khan</h5>
+        <p class="card-text">Associate Professor & HOD
+          <br>
+          Department of Computer Science and Technology
+          <br>
+          Bangladesh Army International University of Science and Technology, Cumilla Cantonment, Bangladesh
+          <br>
+          Phone:
+          <a class="font-size-a" href="tel:01711003233">01711-003233</a>
+          <br>
+          <a class="font-size-a" href="mailto:mak@baiust.edu.bd">mak@baiust.edu.bd</a>
+        </p>
+        <a href="https://cse.baiust.edu.bd/faculty/profile/eyJpdiI6Ikd0MXB5REUyTUN5UlJmWWh1NHZyNkE9PSIsInZhbHVlIjoieisxUHNoZjg2aTRqV0lKdVd2VmRQQT09IiwibWFjIjoiMTYyMzI1OGNmODlmNWIwN2U1OTAxNTJjMTkxMTI3NDYwYTA2MzIwZjEwNDNhNjdmY2RmMzZjNTQ2NzMyZjUxOSJ9" target="_blank" class="btn btn-primary">Details</a>
+      </div>
+    </div>
+    <div class="card border-primary px-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)" >
+    <div class="card-body" style="text-align:center;">
+      <h5 class="card-title">Golam Moktader Nayeem</h5>
+      <p class="card-text">Associate Professor
+        <br>
+        Department of Computer Science and Technology
+        <br>
+        Bangladesh Army International University of Science and Technology, Cumilla Cantonment, Bangladesh
+        <br>
+        Phone:
+        <a class="font-size-a" href="tel:01717293250">01717-293250</a>
+        <br>
+        <a class="font-size-a" href="mailto:gmnayeem@baiust.edu.bd">gmnayeem@baiust.edu.bd</a>
+      </p>
+      <a href="https://cse.baiust.edu.bd/faculty/profile/eyJpdiI6Ikh1ZEVkRU1kcUF4NloybGVzSW54Z2c9PSIsInZhbHVlIjoiVnVBMlJkSjlJcGh3cEdTVjJXeXZ1Zz09IiwibWFjIjoiYWUzYmY2YzE1NTg3MjNhMjc0OTYxNmQxMmQxNGVhNzVhNDViNjM0NzUyYTEzZTg1OTcwYmUzYzBkOTcwMmU4NSJ9" target="_blank" class="btn btn-primary">Details</a>
+    </div>
+  </div>
+  <div class="card border-primary px-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2)">
+  <div class="card-body" style="text-align:center;">
+    <h5 class="card-title">Khalil Ahammad</h5>
+    <p class="card-text">Lecturer
+      <br>
+      Department of Computer Science and Technology
+      <br>
+      Comilla University, Kotbari, Cumilla-3506
+      <br>Phone:
+      <a class="font-size-a" href="tel:01763061626">01763-061626</a>
+      <br>
+      <a class="font-size-a" href="mailto:khalil.cou.cse@gmail.com">khalil.cou.cse@gmail.com</a>
+    </p>
+    <a href="https://cou.ac.bd/cse/khalil_ahammad" target="_blank" class="btn btn-primary">Details</a>
+  </div>
+</div>
+</div>
+</div>
+
+  </body>
+</html>
 <?php include 'lib/footer.php'; ?>
